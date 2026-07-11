61c1206775042f895dbaf7e3748d8a886df4a8fe
Debjit Chatterjee <debjitchatterjee1729@gmail.com>
2026-07-11 21:10:54 +0530

    add user and video model

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,8 +1,8 @@
 class ApiResponse {
-    constructor(stausCode, data, message = "Success"){
+    constructor(statusCode, data, message = "Success"){
         this.stausCode = stausCode
         this.data = data
         this.message = message
-        this.success = stausCode < 400
+        this.success = statusCode < 400
     }
 }
